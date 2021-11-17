fc92a7c3c4d059736ff31b9b79b1fb7b410c8c63
Priyamythreyi <89632129+Priyamythreyi@users.noreply.github.com>
2021-11-17 10:41:03 +0530

    Create generic demo.java

---
+++ generic demo.java
@@ -0,0 +1,43 @@
+//genericdemo.java
+class MyClass<T extends Comparable<T>> 
+{ 
+T[] vals; 
+MyClass(T[] o) 
+{ 
+vals = o; 
+} 
+public T min() 
+{ 
+T v = vals[0]; 
+for(int i=1; i < vals.length; i++) 
+if(vals[i].compareTo(v) < 0) 
+v = vals[i]; 
+return v; 
+} 
+public T max() 
+{ 
+T v = vals[0]; 
+for(int i=1; i < vals.length;i++) 
+if(vals[i].compareTo(v) > 0) 
+v = vals[i]; 
+return v; 
+} 
+} 
+public class genericdemo 
+{ 
+public static void main(String args[]) { 
+int i; 
+Integer inums[]={3,5,4,4,6,1}; 
+Character chs[]={'v','s','t','a','n','h'}; 
+Double d[]={20.2,45.4,71.6,78.3,54.6,8.4}; 
+MyClass<Integer> iob = new MyClass<Integer>(inums); 
+MyClass<Character> cob = new MyClass<Character>(chs); 
+MyClass<Double>dob = new MyClass<Double>(d); 
+System.out.println("Max value in inums: " + iob.max()); 
+System.out.println("Min value in inums: " + iob.min()); 
+System.out.println("Max value in chs: " + cob.max()); 
+System.out.println("Min value in chs: " + cob.min()); 
+System.out.println("Max value in chs: " + dob.max()); 
+System.out.println("Min value in chs: " + dob.min()); 
+} 
+}
